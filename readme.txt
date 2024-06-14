ffcb47250abd421a7a1050980fba9f943bf08e72
CesarMaxLopez <160643427+CesarMaxLopez@users.noreply.github.com>
2024-06-14 01:45:26 +0000

    Initial commit

---
+++ readme.txt
@@ -0,0 +1,14 @@
+DE:numero01, numero02
+DS:numeroMayor
+
+Iniciar sesion 
+declarar numero1
+declarar numero2
+ingresar numero 1
+ingresar numero 2
+si numero1 es mayor que numero2 imprimir 
+  el numero mayor es: NaN(Numero 1)
+sino
+  el numero mayor es: NaN(Numero 2)
+fin de si
+Finalizar sesion 
---
+++ script.js
@@ -0,0 +1,12 @@
+var numero01;
+var numero02;
+numero01=parseInt(prompt("Ingresa el numero 1",""));
+numero02=parseInt(prompt("Ingresa el numero 2",""));
+if (numero01>numero02)
+{
+  document.write("El numero mayor es:" + numero01 + "(Numero 1)");
+}
+else
+   {
+     document.write("El numero menor es:" + numero02 + "(Numero 2)");
+   }
